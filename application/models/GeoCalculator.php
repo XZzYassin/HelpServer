25a0c5386183a18db185b334616f527436dab51d
XZzYassin <yassin.mumen@gmail.com>
2015-04-29 17:18:20 +0300

    nothing important

--- application/models/GeoCalculator.php
+++ application/models/GeoCalculator.php
@@ -1,20 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class GeoCalcolator extends CI_Model
-{
-    /*
-    public function getDistance($lon1, $lat1, $lon2, $lat2)
-    {
-        $theta = $lon1 - $lon2;
-        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +
-                cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-        $dist = acos($dist);
-        $dist = rad2deg($dist);
-        $miles = $dist * 60 * 1.1515;
-        return ($miles * 1.609344);
-    }
-    */
-    // There is no use class anymore.
-    // The class has been converted into MYSQL procedures.
-}
+// There is no use class anymore.
+// The class has been converted into MYSQL procedures.
+
+//class GeoCalcolator extends CI_Model
+//{
+//    public function getDistance($lon1, $lat1, $lon2, $lat2)
+//    {
+//        $theta = $lon1 - $lon2;
+//        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +
+//                cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+//        $dist = acos($dist);
+//        $dist = rad2deg($dist);
+//        $miles = $dist * 60 * 1.1515;
+//        return ($miles * 1.609344);
+//    }
+//}
