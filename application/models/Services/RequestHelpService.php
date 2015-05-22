63a52c046fc40e04338694cc8dc1224a2065ec3b
XZzYassin <yassin.mumen@gmail.com>
2015-05-22 18:10:48 +0300

    RequestHelpService fixed Bugs

--- application/models/Services/RequestHelpService.php
+++ application/models/Services/RequestHelpService.php
@@ -3,25 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class RequestHelpService extends CI_Model implements ExecutableService
 {
-    private $paramsRequired = 5;
+    private $paramsRequired = 7;
     public function execute($request)
     {
         $params = $request->getParameters();
         if(is_array($params) &&  count($params) == $this->paramsRequired)
         {
-            $lon = $params[2];
-            $lat = $params[3];
-            $this->db->insert_id();
             $this->load->helper('date');
-            $ok = $this->db->insert('helps',array(
-                                            'title'=>$params[1],
-                                            'logitude'=>$lon,
-                                            'latitude'=>$lat,
-                                            'time_created'=>now(),
-                                            'category'=>$params[4],
-                                            'status'=>1
-                                            ));
-            $result = array('done'=>$ok); 
+            $created = $this->db->insert('helps',array(
+                                                    'title'=>$params[1],
+                                                    'logitude'=>$params[2],
+                                                    'latitude'=>$params[3],
+                                                    'time_created'=>now(),
+                                                    'category'=>$params[4],
+                                                    'description'=>$params[5],
+                                                    'status'=>1
+                                                 ));
+            $help_id = $this->db->insert_id();
+            $asked = $this->db->insert('asked_helps',array(
+                                                        'asker_id'=>$params[6],
+                                                        'help_id'=>$help_id
+                                                     ));
+            $result = array('done'=>$created && $asked);
+
             return $result;
         }
     }
